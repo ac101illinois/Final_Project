cf080ef34d0b791b936b83e5311aa964d6c0df2a
Abby Circelli <ac101@illinois.edu>
2025-11-24 22:53:43 -0600

    Created models

--- library/models.py
+++ library/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    total_pages = models.IntegerField()
+    pages_read = models.IntegerField(default=0)
+    list_name = models.CharField(max_length=100, default="My Books")
+    def __str__(self):
+        return self.title
+
+class ReadingStats(models.Model):
+    pages_read = models.IntegerField(default=0)
+    books_read = models.IntegerField(default=0)
+    points = models.IntegerField(default=0)
