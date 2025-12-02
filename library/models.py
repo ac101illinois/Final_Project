8d7c0e6aa8183f67b6681851d1ef4ed9ba0992fc
Abby Circelli <ac101@illinois.edu>
2025-12-01 20:17:34 -0600

    Finalized Models

--- library/models.py
+++ library/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class BookList(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     total_pages = models.IntegerField()
     pages_read = models.IntegerField(default=0)
-    list_name = models.CharField(max_length=100, default="My Books")
+    list_name = models.ForeignKey(BookList, on_delete=models.SET_NULL, null=True)
     def __str__(self):
         return self.title
 
@@ -14,3 +20,13 @@ class ReadingStats(models.Model):
     pages_read = models.IntegerField(default=0)
     books_read = models.IntegerField(default=0)
     points = models.IntegerField(default=0)
+
+class Reward(models.Model):
+    reward = models.CharField(max_length=100)
+    required_points = models.IntegerField()
+
+    def __str__(self):
+        return self.reward
+
+
+
--- library/views.py
+++ library/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Book, ReadingStats
 
 # Create your views here.
+#Views: search for book, add, delete, update progress,
+
