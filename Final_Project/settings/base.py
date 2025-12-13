2b38e88f2ffb93157b1477c34c8e7222e8f04754
Abby Circelli <ac101@illinois.edu>
2025-12-12 18:24:29 -0600

    settings files, collectstaic and requirements.txt were created

--- Final_Project/settings/base.py
+++ Final_Project/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'Final_Project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'data' / 'db.sqlite3',
     }
 }
 
@@ -117,13 +117,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS = [BASE_DIR / "static"]
 
 LOGIN_URL = 'library:login'
 LOGIN_REDIRECT_URL = 'library:home-view'
---
+++ Final_Project/settings/production.py
@@ -0,0 +1,18 @@
+from .base import *
+
+DEBUG = True
+
+# Enter your PythonAnywhere account here in the allowed hosts.
+# Replace it with your name:
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ac101.pythonanywhere.com']
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'ac101$info390mg-mysql-database',
+#         'USER': 'ac101',
+#         'PASSWORD': 'funklibrary',
+#         'HOST': 'ac101.mysql.pythonanywhere-services.com',
+#         'PORT': '3306',
+#     }
+# }
